3eca4f6ccb61cd51c7e5a68e428185d588782bd1
José Malcher Jr <contato@josemalcher.net>
2022-02-15 22:50:49 -0300

    04 - Renderização Condicional no Vue 3

--- 02-Basic/app.js
+++ 02-Basic/app.js
@@ -5,13 +5,16 @@ Vue.createApp({
             product: {
                 title: "Filme 1",
                 desc: "Filme 1 descrição",
-                image: './assets/filme01.jpg'
+                image: './assets/filme01.jpg',
+                stars: 5
             },
             product2: {
                 title: "Filme 12",
                 desc: "Filme 2 descrição",
-                image: './assets/filme02.jpg'
-            }
+                image: './assets/filme02.jpg',
+                stars: 5
+            },
+            number: 0
         }
     }
 }).mount('#app')
