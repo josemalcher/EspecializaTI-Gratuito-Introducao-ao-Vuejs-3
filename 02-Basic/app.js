c71c39617bf0fa72201e20a6081cd0b99ec57bf5
José Malcher Jr <contato@josemalcher.net>
2022-02-15 22:34:29 -0300

    03 - Entendendo Sobre Bidding no Vue 3

--- 02-Basic/app.js
+++ 02-Basic/app.js
@@ -1,7 +1,17 @@
 Vue.createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            product: {
+                title: "Filme 1",
+                desc: "Filme 1 descrição",
+                image: './assets/filme01.jpg'
+            },
+            product2: {
+                title: "Filme 12",
+                desc: "Filme 2 descrição",
+                image: './assets/filme02.jpg'
+            }
         }
     }
 }).mount('#app')
