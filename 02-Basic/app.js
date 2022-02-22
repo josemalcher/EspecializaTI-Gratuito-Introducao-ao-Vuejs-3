facf8982f7e618215e3bc43ade2bb82cbca90fa3
José Malcher Jr <contato@josemalcher.net>
2022-02-21 22:57:37 -0300

    05 - Loops no Vue 3 (v-for)

--- 02-Basic/app.js
+++ 02-Basic/app.js
@@ -2,19 +2,21 @@ Vue.createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            product: {
-                title: "Filme 1",
-                desc: "Filme 1 descrição",
-                image: './assets/filme01.jpg',
-                stars: 5
-            },
-            product2: {
-                title: "Filme 12",
-                desc: "Filme 2 descrição",
-                image: './assets/filme02.jpg',
-                stars: 5
-            },
-            number: 0
+            number: 0,
+            products: [
+                {
+                    title: "Filme 1",
+                    desc: "Filme 1 descrição",
+                    image: './assets/filme01.jpg',
+                    stars: 5
+                },
+                {
+                    title: "Filme 12",
+                    desc: "Filme 2 descrição",
+                    image: './assets/filme02.jpg',
+                    stars: 5
+                }
+            ]
         }
     }
 }).mount('#app')
